af1d4dd12a02a60c51f01df8174227bfcb101ea3
heitorgusan <heitorgusan@gmail.com>
2022-07-18 21:20:45 -0300

    post

--- src/main/java/com/cursos/crud/cursos_crud/controller/CourseController.java
+++ src/main/java/com/cursos/crud/cursos_crud/controller/CourseController.java
@@ -1,8 +1,14 @@
 package com.cursos.crud.cursos_crud.controller;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursos.crud.cursos_crud.model.Course;
@@ -24,5 +30,14 @@ public class CourseController {
 	public List<Course> list(){
 		return this.courseRepository.findAll();
 	}
+	
+	@PostMapping
+	@ResponseStatus(code= HttpStatus.CREATED)
+	public Course create(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+	//public ResponseEntity<Course>create(@RequestBody Course course){
+		//return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+	//}
 	
 }
